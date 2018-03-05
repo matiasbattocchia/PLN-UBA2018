2576f25ed4fe3f37ef477789f780738fb23b7421
Matías Battocchia <matias@riseup.net>
2018-03-05 08:24:59 -0300

    práctico 1, ejercicios 2 y 4, modelos de n-gramas
    Pasan los tests.

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -38,7 +38,17 @@ class LanguageModel(object):
         return math.pow(2.0, self.cross_entropy(sents))
 
 
+from collections import Counter
+import numpy as np
+
 class NGram(LanguageModel):
+
+    def ngrams(self, n, sent):
+        # marcadores de comienzo y fin de oración
+        _sent_ = ['<s>'] * (self._n - 1) + sent + ['</s>']
+
+        for i in range(len(_sent_) - self._n + 1):
+            yield tuple(_sent_[i:i+n])
 
     def __init__(self, n, sents):
         """
@@ -47,41 +57,58 @@ class NGram(LanguageModel):
         """
         assert n > 0
         self._n = n
+        self.n0_counter = Counter()
+        self.n1_counter = Counter()
 
-        count = defaultdict(int)
-
-        # WORK HERE!!
-
-        self._count = dict(count)
+        for sent in sents:
+            # contando n-gramas y (n-1)-gramas
+            self.n0_counter.update(self.ngrams(n,   sent))
+            self.n1_counter.update(self.ngrams(n-1, sent))
 
     def count(self, tokens):
         """Count for an n-gram or (n-1)-gram.
 
         tokens -- the n-gram or (n-1)-gram tuple.
         """
-        return self._count.get(tokens, 0)
+        if len(tokens) == self._n:
+            return self.n0_counter[tokens]
+        elif len(tokens) == self._n - 1:
+            return self.n1_counter[tokens]
 
-    def cond_prob(self, token, prev_tokens=None):
+    def cond_prob(self, token, prev_tokens=tuple()):
         """Conditional probability of a token.
 
         token -- the token.
         prev_tokens -- the previous n-1 tokens (optional only if n = 1).
         """
-        # WORK HERE!!
+        return self.cond_prob_ngram( prev_tokens + (token,) )
+
+    def cond_prob_ngram(self, ngram):
+        """Conditional probability of a token.
+
+        tokens  -- tuple of tokens; i.e. (n-2, n-1, n)
+        returns -- p(n | n-1, n-2) = count(n-2, n-1, n) / count(n-2, n-1)
+        """
+        prob = np.divide( self.n0_counter[ngram], self.n1_counter[ngram[:-1]] )
+
+        if np.isnan(prob):
+            return 0.0
+        else:
+           return prob
 
     def sent_prob(self, sent):
         """Probability of a sentence. Warning: subject to underflow problems.
 
         sent -- the sentence as a list of tokens.
         """
-        # WORK HERE!!
+        return np.prod( list(map( self.cond_prob_ngram, self.ngrams(self._n, sent) )) )
 
     def sent_log_prob(self, sent):
         """Log-probability of a sentence.
 
         sent -- the sentence as a list of tokens.
         """
-        # WORK HERE!!
+        return np.sum( np.log2( list(map( self.cond_prob_ngram, self.ngrams(self._n, sent) )) ) )
 
 
 class AddOneNGram(NGram):
@@ -95,29 +122,22 @@ class AddOneNGram(NGram):
         super().__init__(n, sents)
 
         # compute vocabulary
-        self._voc = voc = set()
-        # WORK HERE!!
+        self._voc = set( [ngram[-1] for ngram in self.n0_counter.keys()] )
 
-        self._V = len(voc)  # vocabulary size
+        self._V = len(self._voc)  # vocabulary size
 
     def V(self):
         """Size of the vocabulary.
         """
         return self._V
 
-    def cond_prob(self, token, prev_tokens=None):
+    def cond_prob_ngram(self, ngram):
         """Conditional probability of a token.
 
-        token -- the token.
-        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        tokens  -- tuple of tokens; i.e. (n-2, n-1, n)
+        returns -- p(n | n-1, n-2) = count(n-2, n-1, n) / count(n-2, n-1)
         """
-        n = self._n
-        if not prev_tokens:
-            # if prev_tokens not given, assume 0-uple:
-            prev_tokens = ()
-        assert len(prev_tokens) == n - 1
-
-        # WORK HERE!!
+        return np.divide( self.n0_counter[ngram] + 1, self.n1_counter[ngram[:-1]] + self._V)
 
 
 class InterpolatedNGram(NGram):
